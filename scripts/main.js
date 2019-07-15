e7fc73e8e0f8deb1a1e33bea4d16dfe23b817e2e
mjimeneztn <mjimeneztn@gmail.com>
2019-07-15 14:29:27 -0500

    created js file for practice

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+let bandNumber = 1
+const takeNumber = function (bandName) {
+    console.log(`${bandNumber} ${bandName}`);
+return bandNumber++}
+
+
+
+/*
+    Write your awesome code here. See comments
+    below for what should be returned.
+*/
+
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)
+const toke = takeNumber("Toke Croix")
+console.log(toke)  // T
